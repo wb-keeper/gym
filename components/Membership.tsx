87da502069de228a0f74a780663f810b21f49b1b
wb-keeper <wb-keeper@mail.ru>
2024-05-20 19:03:51 +0300

    Working on Membership section

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { motion } from "framer-motion";
+import MembershipSlider from "@/components/MembershipSlider";
+
 const Membership = () => {
   return (
-    <section className="h-screen bg-purple-300" id="prices">
-      Membership
+    <section
+      className="py-8 xl:py-0 lg:h-[95vh] bg-membership bg-cover bg-center relative"
+      id="prices"
+    >
+      <div className="container mx-auto px-0 text-white h-full flex flex-col xl:pt-24 relative z-20">
+        <h2 className="h2 text-white text-center mb-8">Membership</h2>
+        <div>
+          <MembershipSlider />
+        </div>
+      </div>
     </section>
   );
 };
---
+++ components/MembershipSlider.tsx
@@ -0,0 +1,140 @@
+"use client";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FaCheck } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
+import { Pagination } from "swiper/modules";
+
+const membershipData = [
+  {
+    title: "Standard",
+    price: "30",
+    benefits: [
+      {
+        icon: FaCheck,
+        text: "Includes membership",
+      },
+      {
+        icon: FaCheck,
+        text: "Access to all gym facilities",
+      },
+      {
+        icon: MdClose,
+        text: "Diet plan included",
+      },
+      {
+        icon: FaCheck,
+        text: "Health and fitness tips",
+      },
+      {
+        icon: MdClose,
+        text: "Monday-Friday gym access",
+      },
+      {
+        icon: FaCheck,
+        text: "Full access to everything",
+      },
+      {
+        icon: MdClose,
+        text: "No additional amenities",
+      },
+    ],
+  },
+  {
+    title: "Ultimate",
+    price: "45",
+    benefits: [
+      {
+        icon: FaCheck,
+        text: "Includes membership",
+      },
+      {
+        icon: FaCheck,
+        text: "Access to all gym facilities",
+      },
+      {
+        icon: FaCheck,
+        text: "Diet plan included",
+      },
+      {
+        icon: FaCheck,
+        text: "Health and fitness tips",
+      },
+      {
+        icon: FaCheck,
+        text: "Monday-Friday gym access",
+      },
+      {
+        icon: FaCheck,
+        text: "Full access to everything",
+      },
+      {
+        icon: MdClose,
+        text: "No additional amenities",
+      },
+    ],
+  },
+  {
+    title: "Professional",
+    price: "60",
+    benefits: [
+      {
+        icon: FaCheck,
+        text: "Includes membership",
+      },
+      {
+        icon: FaCheck,
+        text: "Access to all gym facilities",
+      },
+      {
+        icon: FaCheck,
+        text: "Diet plan included",
+      },
+      {
+        icon: FaCheck,
+        text: "Health and fitness tips",
+      },
+      {
+        icon: FaCheck,
+        text: "Monday-Friday gym access",
+      },
+      {
+        icon: FaCheck,
+        text: "Full access to everything",
+      },
+      {
+        icon: FaCheck,
+        text: "No additional amenities",
+      },
+    ],
+  },
+];
+const MembershipSlider = () => {
+  return (
+    <Swiper
+      slidesPerView={1}
+      modules={[Pagination]}
+      pagination={{
+        clickable: true,
+      }}
+      breakpoints={{
+        768: { slidesPerView: 2, spaceBetween: 30 },
+        1024: { slidesPerView: 3, spaceBetween: 30 },
+      }}
+      className="min-h-[680px]"
+    >
+      {membershipData.map((item, index) => (
+        <SwiperSlide key={index}>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+            beatae deleniti dolores dolorum iusto labore magnam magni minima
+            quod soluta.
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default MembershipSlider;
