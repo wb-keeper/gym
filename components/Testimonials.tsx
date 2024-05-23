e7aa7c447bbf129f73a4a9a42a8e0c3336133409
wb-keeper <wb-keeper@mail.ru>
2024-05-23 19:52:37 +0300

    Small changes

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,7 +1,22 @@
+"use client";
+import "swiper/css";
+import "swiper/css/pagination";
+import { motion } from "framer-motion";
+const testimonialData = [
+  {
+    img: "/testimonial/lucy.jpg",
+    message:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, at consectetur debitis, ducimus est fuga harum incidunt ipsam molestia.",
+    name: "Lucy Anthony",
+  },
+];
 const Testimonials = () => {
   return (
     <section className="h-[50vh] bg-teal-300" id="testimonial">
-      Testimonials
+      Testimonials Lorem ipsum dolor sit amet, consectetur adipisicing elit. A,
+      at consectetur debitis, ducimus est fuga harum incidunt ipsam molestias
+      mollitia nesciunt nisi nostrum nulla quidem quis quos repellat similique,
+      veritatis.
     </section>
   );
 };
